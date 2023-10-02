dea9b59fc68a11a5f370b5e23e9a4f485114cbbf
Michael Barrientos <michael@union.ai>
2023-10-01 22:57:41 -0700

    dashboards use Grafana golang api

--- pkg/grafana/utils.go
+++ pkg/grafana/utils.go
@@ -2,34 +2,7 @@ package grafana
 
 import (
 	"encoding/json"
-	"regexp"
-
-	gclient "github.com/grafana/grafana-openapi-client-go/client"
 )
-
-var folderURLRegex = regexp.MustCompile("/dashboards/f/([^/]+)")
-
-const generalFolderId = 0
-const generalFolderUID = "general"
-
-func extractFolderUID(client *gclient.GrafanaHTTPAPI, d DashboardWrapper) string {
-	folderUid := d.Meta.FolderUID
-	if folderUid == "" {
-		urlPaths := folderURLRegex.FindStringSubmatch(d.Meta.FolderURL)
-		if len(urlPaths) == 0 {
-			if d.FolderID == generalFolderId {
-				return generalFolderUID
-			}
-			folder, err := getFolderById(client, d.FolderID)
-			if err != nil {
-				return ""
-			}
-			return folder.UID
-		}
-		folderUid = urlPaths[1]
-	}
-	return folderUid
-}
 
 func structToMap(s interface{}) (map[string]interface{}, error) {
 	jsonData, err := json.Marshal(s)
